26d61ad2628da29272676b060688dd6ebaff02a1
Brian <linbrian24@gmail.com>
2023-03-09 09:01:17 -0500

    Complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = (start, end) => {
+    // Edge cases.
+    if (start < 0 || end < 0) {
+        return "ERROR"
+    }
 
+    if (typeof start !== "number" || typeof end !== "number") {
+        return "ERROR";
+    }
+
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+
+    let sumOfAll = 0;
+
+    for (let i = start; i <= end; i++) {
+        sumOfAll += i;
+    }
+
+    return sumOfAll;
 };
 
 // Do not edit below this line
