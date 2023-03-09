5adc795020a97051233a97dc5e892c2484407b74
Brian <linbrian24@gmail.com>
2023-03-09 08:49:41 -0500

    Complete removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const clone = [];
+    let index = 0;
 
+    for (let i = 0; i < array.length; i++) {
+        if (args.includes(array[i])) {
+            continue;
+        }
+
+        clone[index++] = array[i];
+    }
+
+    return clone;
 };
 
 // Do not edit below this line
