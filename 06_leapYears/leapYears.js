4f33ed7a587b23ba7a0c8a4e70dad41f117baa41
Brian <linbrian24@gmail.com>
2023-03-11 12:30:21 -0500

    Complete leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,26 @@
-const leapYears = function() {
+const leapYears = (year) => {
+    // Leap years are years divisible by four (like 1984 and 2004). 
+    // However, years divisible by 100 are not leap years (such as 1800 and 1900) 
+    // unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years). (Yes, it's all pretty confusing)
+    if (year % 400 === 0 && year % 100 === 0) {
+        // Leap year.
+        return true;
+    }
 
+    else if (year % 100 === 0) {
+        // Not a leap year.
+        return false;
+    }
+
+    else if (year % 4 === 0) {
+        // Leap year.
+        return true;
+    }
+
+    else {
+        // Not a leap year.
+        return false;
+    }
 };
 
 // Do not edit below this line
