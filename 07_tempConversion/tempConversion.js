9ee8aafe2dc595e1036a472bbe9c231c60978043
Brian <linbrian24@gmail.com>
2023-03-11 12:38:26 -0500

    Complete tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = (fahrenheit) => {
+  // Convert using the formula.
+  let celsius = (fahrenheit - 32) * (5/9);
+
+  // Rounds to nearest decimal.
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = (celsius) => {
+  // Convert using the formula.
+  let fahrenheit = celsius * (9/5) + 32;
+
+  // Rounds to nearest decimal.
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
