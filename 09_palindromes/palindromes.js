3fa3f89e17e51ac0925aeb9d71a6766f47d104e7
Brian <linbrian24@gmail.com>
2023-03-17 19:42:05 -0400

    Finish palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = (word) => {
+    // Filter out lowercase, punctuation, and spaces.
+    let filtered = word.toLowerCase().replace(/[^a-z]/g, "");
 
+    // Splits into array, uses reverse method to swap all the indices, rejoins into a String and compares.
+    return filtered.split("").reverse().join("") === filtered;
 };
 
 // Do not edit below this line
