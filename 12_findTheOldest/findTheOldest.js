0070cd0fde86dc2b34f5a0bef7568af318f7cd0f
Brian <linbrian24@gmail.com>
2023-03-17 19:38:58 -0400

    Complete findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = (array) => {
+    const oldest = array.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
 
+        return oldestAge > currentAge ? oldest : current;
+    })
+
+    return oldest;
+};
+
+const getAge = (birth, death) => {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+
+    return death - birth;
 };
 
 // Do not edit below this line
